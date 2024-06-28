30efb2aae53a8a9f3f204ce0ac41b5a5b423aa4a
Eloi Stree <eloistree@gmail.com>
2024-06-28 05:00:54 +0200

    Save: 06/28/2024 5:00 AM

--- Runtime/EventAndIndexFromToServerTunnelingRsaMono.cs
+++ Runtime/EventAndIndexFromToServerTunnelingRsaMono.cs
@@ -11,19 +11,33 @@ public class EventAndIndexFromToServerTunnelingRsaMono : MonoBehaviour{
     public UnityEvent<bool> m_onIndexLockChangedAndIsValide;
     public UnityEvent<string> m_publicKeyValidated;
 
+
+    private bool m_connected = false;
+    private bool m_disconnected = false;
+    private bool m_handshakeVerified = false;
+    private bool m_indexLockChanged = false;
+    private int m_indexLock = 0;
+    private string m_publicKey = "";
     private void Awake()
     {
-        m_source.m_trafficEvent.m_onConnectionSignedAndValidated += m_onHandshakeVerified.Invoke;
-        m_source.m_trafficEvent.m_onWebsocketConnected += m_onDisconnected.Invoke;
-        m_source.m_trafficEvent.m_onConnectionLost += m_onConnected.Invoke;
-        m_source.m_trafficEvent.m_onIndexLockChanged += LockerChanged;
+        m_source.m_trafficEvent.m_onConnectionSignedAndValidated += () => { m_handshakeVerified = true; };
+        m_source.m_trafficEvent.m_onWebsocketConnected += () => { m_handshakeVerified = true; };
+        m_source.m_trafficEvent.m_onConnectionLost += () => { m_handshakeVerified = true; };
+        m_source.m_trafficEvent.m_onIndexLockChanged += (t) => { m_indexLock = t; m_indexLockChanged = true; };
 
     }
-
-    private void LockerChanged(int obj)
+    private void Update()
     {
-        m_onIndexLockChanged.Invoke(obj);
-        m_onIndexLockChangedAndIsValide.Invoke(obj >int.MinValue);
-        m_publicKeyValidated.Invoke(m_source.m_tunnel.m_useKeyPair.m_publicKey.Trim().Replace("\n", "").Replace("  ", " "));
-    }
+        if (m_connected == true) { m_connected = false; m_onConnected.Invoke(); }
+        if (m_disconnected == true) { m_disconnected = false; m_onDisconnected.Invoke(); }
+        if (m_handshakeVerified == true) { m_handshakeVerified = false; m_onHandshakeVerified.Invoke(); }
+        if (m_indexLockChanged == true) { m_indexLockChanged = false;
+            bool isInvalide = m_indexLock == int.MinValue;
+            m_onIndexLockChanged.Invoke(m_indexLock);
+            m_onIndexLockChangedAndIsValide.Invoke(!isInvalide);
+            m_publicKeyValidated.Invoke(m_source.m_tunnel.GetPublicXmlKey());
+        
+        }
+         }
+
 }
